a42275c02919dbad00737013d3bb1003fa5c0299
sthelo <ngidisthelo@gmail.com>
2024-03-22 16:05:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ function displayTemperature(response) {
     let temperatureElement = document.querySelector("#current-temperature");
     let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time *1000);
+    let iconElement = document.querySelector("#icon");
+    
+    
+    
+    iconElement.innerHTML =`<img src ="${response.data.condition.icon_url}"
+    class="current-temperature-icon"/>`
+
+    console.log(response.data.condition.description);
+
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
     temperatureElement.innerHTML = temperature;
   }
   
@@ -16,39 +34,21 @@ function displayTemperature(response) {
   
     axios.get(apiUrl).then(displayTemperature);
   }
-  
-  function formatDate(date) {
+  function formatDate(date){
+    
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let day = date.getDay();
-  
-    if (minutes < 10) {
+    let days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday"];
+    let day = days[date.getDay()];
+
+    if (minutes< 10){
       minutes = `0${minutes}`;
     }
-  
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-  
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-  
-    let formattedDay = days[day];
-    return `${formattedDay} ${hours}:${minutes}`;
+
+    return `${day} ${hours}:${minutes}`;
   }
+  
+
   
   let searchForm = document.querySelector("#search-form");
   searchForm.addEventListener("submit", search);
-  
-  let currentDateELement = document.querySelector("#current-date");
-  let currentDate = new Date();
-  
-  currentDateELement.innerHTML = formatDate(currentDate);
-  
